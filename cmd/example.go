e999969a7ec3e923f1fc87d92760587c3e6de696
jneo8 <jneo8lin@gmail.com>
2020-11-09 01:06:29 +0800

    feat(cmd/example.go): example server setup

---
+++ cmd/example.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jneo8/rate-limit-server-go/repository/tokenbucket"
+	"log"
+	"strconv"
+)
+
+func main() {
+	r := gin.Default()
+	tokenbucket := tokenbucket.New(60)
+
+	go func() {
+		if err := tokenbucket.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	r.GET("/", func(c *gin.Context) {
+		ip := c.ClientIP()
+		n := tokenbucket.Get(ip)
+		if n >= 60 {
+			c.String(200, "error")
+		} else {
+			c.String(200, strconv.Itoa(n))
+		}
+	})
+	r.Run()
+}
